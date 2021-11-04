98824a74576a13f39583be0d2f8d4ac5b3fc9837
akbardevelopment <akbardaminov51@gmail.com>
2021-11-04 23:32:55 +0500

    first

---
+++ js/dist/style.dev.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var player = document.querySelector("lottie-player");
+var play = document.querySelector(".congratulations");
+$(".click").click(function () {
+  $(".congratulations").show();
+  player.play();
+});
+$(".hambur").click(function () {
+  $(".PIRST").show(1000);
+  $(".letter").hide();
+});
+$(".set").click(function () {
+  $(".settings").fadeIn();
+  $(".set").click(function () {
+    $(".settings").fadeOut();
+  });
+});
+$(document).ready(function () {
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 2000
+  });
+});
